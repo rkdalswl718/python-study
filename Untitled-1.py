055343dfa872be5624f377aa11d440b6541c5f7c
rkdalswl718 <rkdalswl070718@gmail.com>
2023-08-17 10:34:22 +0900

    코드업 6088

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,11 +1,3 @@
-r,g,b = input().split()
-r=int(r)
-g=int(g)
-b=int(b)
+a,d,n=map(int, input().split())
 
-for i in range(0, r) :
-  for j in range(0, g) :
-    for k in range(0, b) :
-      print(i, j, k)
-
-print(r*g*b)
+print(a+d*(n-1))
