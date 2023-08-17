5cd7d2daeb974fab35ba944533f908416c691334
rkdalswl718 <rkdalswl070718@gmail.com>
2023-08-17 10:11:16 +0900

    코드업 6073

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,10 +1,5 @@
 a = int(input())
 
-if a in [12, 1, 2]:
-    print("winter")
-elif a in [3, 4, 5]:
-    print("spring")
-elif a in [6, 7, 8]:
-    print("summer")
-else:
-    print("fall")
+while a!=0:
+    a=a-1
+    print(a)
