1a823805d73e2a06b6e2043bc59ed6395b3927ab
rkdalswl718 <rkdalswl070718@gmail.com>
2023-08-17 09:23:35 +0900

    while

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,12 +1,6 @@
-print("hello world!")
-a=int(input("정수를 입력하세요"))
-b=int(input())
-print(a+b)
-print(type(a+b))
-
-#bool형
-a=True
-if (a):
-    print("참")
-else :
-    print("거짓")
+a=int(input())
+while True:
+    print(a)
+    a+=1
+    if a>1:
+        break
