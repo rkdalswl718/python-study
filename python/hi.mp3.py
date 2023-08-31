362da08475051a0a198bf292c3f980e75bc3697b
rkdalswl718 <rkdalswl070718@gmail.com>
2023-08-31 10:36:23 +0900

    코드업6096

--- python/hi.mp3.py
+++ python/hi.mp3.py
@@ -1,15 +1,26 @@
-d=[]                        #대괄호 [ ] 를 이용해 아무것도 없는 빈 리스트 만들기
+d=[] #대괄호 [ ] 를 이용해 아무것도 없는 빈 리스트 만들기
+
 for i in range(20) :
-  d.append([])         #리스트 안에 다른 리스트 추가해 넣기
-  for j in range(20) : 
-    d[i].append(0)    #리스트 안에 들어있는 리스트 안에 0 추가해 넣기
+    d.append([])         #리스트 안에 다른 리스트 추가해 넣기
+    for j in range(20) : 
+        d[i].append(0)    #리스트 안에 들어있는 리스트 안에 0 추가해 넣기
 
 n = int(input())
+
 for i in range(n) :
-  x, y = input().split()
-  d[int(x)][int(y)] = 1
+    x,y=input().split()
+    for j in range(1, 20) :
+        if d[j][int(y)]==0 :
+            d[j][int(y)]=1
+    else :
+        d[j][int(y)]=0
+
+    if d[int(x)][j]==0 :
+        d[int(x)][j]=1
+    else :
+        d[int(x)][j]=0
 
 for i in range(1, 20) :
-  for j in range(1, 20) : 
-    print(d[i][j], end=' ')    #공백을 두고 한 줄로 출력
-  print()            
+    for j in range(1, 20) : 
+        print(d[i][j], end=' ')    #공백을 두고 한 줄로 출력
+print()            
--- python/tempCodeRunnerFile.py
+++ python/tempCodeRunnerFile.py
@@ -1,5 +1,26 @@
-from gtts import gTTS
+d=[] #대괄호 [ ] 를 이용해 아무것도 없는 빈 리스트 만들기
 
-text = "안녕하세요, 부소마입니다.";
-tts = gTTS(text=text, lang='ko');
-tts.save(r"3. 텍스트를 음성으로 변환\hi.mp3");
+for i in range(20) :
+    d.append([])         #리스트 안에 다른 리스트 추가해 넣기
+    for j in range(20) : 
+        d[i].append(0)    #리스트 안에 들어있는 리스트 안에 0 추가해 넣기
+
+n = int(input())
+
+for i in range(n) :
+    x,y=input().split()
+    for j in range(1, 20) :
+        if d[j][int(y)]==0 :
+            d[j][int(y)]=1
+    else :
+        d[j][int(y)]=0
+
+    if d[int(x)][j]==0 :
+        d[int(x)][j]=1
+    else :
+        d[int(x)][j]=0
+
+for i in range(1, 20) :
+    for j in range(1, 20) : 
+        print(d[i][j], end=' ')    #공백을 두고 한 줄로 출력
+print()            
