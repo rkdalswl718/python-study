68d84469a4107f514ed013db0d87a8d96f0446f4
rkdalswl718 <rkdalswl070718@gmail.com>
2023-10-04 12:14:25 +0900

    재귀 계단 오르기

--- python/hi.mp3.py
+++ python/hi.mp3.py
@@ -1,11 +1,26 @@
-n=int(input()) # 계단 개수
-s=[int(input()) for _ in range(n)] # 계단 리스트
-dp=[0]*(n) # dp 리스트
-if len(s)<=2: # 계단이 2개 이하일땐 그냥 다 더해서 출력
-    print(sum(s))
-else: # 계단이 3개 이상일 때
-    dp[0]=s[0] # 첫째 계단 수동 계산
-    dp[1]=s[0]+s[1] # 둘째 계단까지 수동 계산
-    for i in range(2,n): # 3번째 계단 부터 dp 점화식 이용해서 최대값 구하기
-        dp[i]=max(dp[i-3]+s[i-1]+s[i], dp[i-2]+s[i])
-    print(dp[-1])
+def max_score_recursive(n, s):
+    if n <= 2:
+        return sum(s)
+    
+    def helper(i):
+        if i < 0:
+            return 0
+        if i == 0:
+            return s[0]
+        if i == 1:
+            return s[0] + s[1]
+        
+        # 현재 계단을 오를 때 두 가지 옵션을 고려하여 최대값을 계산합니다.
+        option1 = helper(i - 3) + s[i - 1] + s[i]
+        option2 = helper(i - 2) + s[i]
+        
+        # 두 옵션 중에서 더 큰 값을 반환합니다.
+        return max(option1, option2)
+    
+    return helper(n - 1)
+
+n = int(input())  # 계단 개수
+s = [int(input()) for _ in range(n)]  # 계단 리스트
+
+result = max_score_recursive(n, s)
+print(result)
