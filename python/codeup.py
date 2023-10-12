f272d238ff967a3b5d7dd3243fd375b8115557bd
rkdalswl718 <rkdalswl070718@gmail.com>
2023-10-12 10:35:08 +0900

    1618 줄세우기

--- python/codeup.py
+++ python/codeup.py
@@ -1,14 +1,17 @@
-def is_palindrome(num):
-    return str(num) == str(num)[::-1]
+a, b, c = map(int, input().split())
 
-def is_valid_password(num):
-        num_str = str(num)
-        reversed_num_str = num_str[::-1]
-        num = int(num_str) + int(reversed_num_str)
-        if is_palindrome(num):
-            return "YES"
-        return "NO"
-
-num = int(input())
-
-print(is_valid_password(num))
+if a <= b and a <= c:
+    if c <= b:
+        print(a, c, b)
+    else:
+        print(a, b, c)
+elif b <= a and b <= c:
+    if a <= c:
+        print(b, a, c)
+    else:
+        print(b, c, a)
+else:
+    if a <= b:
+        print(c, a, b)
+    else:
+        print(c, b, a)
