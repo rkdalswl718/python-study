485c625da80963eb7e8ca0b92806f66c7ca2bfa8
rkdalswl718 <rkdalswl070718@gmail.com>
2023-10-31 21:46:51 +0900

    6073

--- python/class.py
+++ python/class.py
@@ -1,10 +1,4 @@
 a = int(input())
-
-if(a//3==1):
-    print('spring')
-elif(a//3==2):
-    print('summer')
-elif(a//3==3):
-    print('fall')
-else:
-    print('winter')
+while a !=0:
+    a=a-1
+    print(a)
