106c13ea61186f76f38748bd362b24f34c3ecb04
rkdalswl718 <rkdalswl070718@gmail.com>
2023-10-31 23:09:45 +0900

    6080

--- python/class.py
+++ python/class.py
@@ -1,7 +1,5 @@
-a = int(input())
-s=0
-i=0
-while i<a:
-    s = s+1
-    i = i+s
-print(s)
+n,m = map(int,input().split())
+
+for i in range (1, n+1):
+    for j in range (1, m+1):
+        print(i,j)
