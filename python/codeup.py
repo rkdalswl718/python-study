9febaa57ff0bca25221ff6993962a4844f508304
rkdalswl718 <rkdalswl070718@gmail.com>
2023-11-01 12:27:44 +0900

    stack

--- python/codeup.py
+++ python/codeup.py
@@ -1,12 +1,2 @@
-def Max(list, len):
-  if len == 1:
-    return list[0]
-  maxNum = Max(list, len-1)
-  if maxNum > list[len-1]:
-    return maxNum
-  else:
-    return list[len-1]
-
-list = list(map(int,input().split()))
-
-print(Max(list, len(list)))
+n = int(input())
+a = input().split()
