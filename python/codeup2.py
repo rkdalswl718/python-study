fbf19ae83dbf9400150119b649df68f98e55c041
rkdalswl718 <rkdalswl070718@gmail.com>
2023-11-09 02:32:01 +0900

    1610

--- python/codeup2.py
+++ python/codeup2.py
@@ -1,8 +1,4 @@
-n = int(input())  # n = 3
-a = list(map(int, input().split()))
-
-for i in range(n):
-    for j in range(n):
-        print(a[(i+j)%n],end=' ')
-    print()
-    
+str = input()
+start, length = map(int,input().split())
+result = str[start:start+length]
+print(result)
