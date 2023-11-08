5ea951206bfadee024eb8a27ef37edc008657a82
rkdalswl718 <rkdalswl070718@gmail.com>
2023-11-09 08:11:08 +0900

    3301

--- python/codeup.py
+++ python/codeup.py
@@ -1,8 +1,8 @@
 def f(a,b):
     if (a%b%c == 0):
         return b;
-    return f(b, a%b)
+    return f(b, a%b%c)
 
 a,b,c = map(int, input().split())
 
-print(f(f(a,b),c))
+print(f(f(a,bA),c))
--- python/codeup2.py
+++ python/codeup2.py
@@ -1,6 +1,7 @@
 n = int(input())
-
-while n >= 10:
-    n = sum(map(int, str(n)))
-
-print(n)
+cnt = 0
+for i in [50000, 10000, 5000, 1000, 500, 100, 50, 10]:
+    if n//i != 0:
+        cnt += n//i
+        n = n%i
+print(cnt)
