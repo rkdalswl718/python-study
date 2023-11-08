c133470ef9ac0dd5aa07d974ba87c50233962bec
rkdalswl718 <rkdalswl070718@gmail.com>
2023-11-09 00:35:22 +0900

    6095

--- python/codeup2.py
+++ python/codeup2.py
@@ -1,4 +1,10 @@
+d = [[0] * 20 for _ in range (20)]
 n = int(input())
-a = list(map(int,input().split()))
-for i in range(1) :
-    print(min(a))
+for i in range (n):
+    x,y = map(int,input().split())
+    d[x][y]=1
+
+for i in range(1,20):
+    for j in range(1,20):
+        print(d[i][j], end=' ')    #공백을 두고 한 줄로 출력
+    print()
