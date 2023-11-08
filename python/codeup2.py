9aa1740275ed9db041390b395a45234f7466367b
rkdalswl718 <rkdalswl070718@gmail.com>
2023-11-09 02:27:27 +0900

    1405

--- python/codeup2.py
+++ python/codeup2.py
@@ -1,10 +1,8 @@
-def 이(n):
-    if n == 0:
-        return '0'
-    elif n == 1:
-        return '1'
-    else:
-        return 이(n // 2) + str(n % 2)
+n = int(input())  # n = 3
+a = list(map(int, input().split()))
 
-n = int(input())
-print(이(n))
+for i in range(n):
+    for j in range(n):
+        print(a[(i+j)%n],end=' ')
+    print()
+    
