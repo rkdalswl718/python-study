3bc6fe34ed542bfa61a31b48777bcd1a2be7e651
rkdalswl718 <rkdalswl070718@gmail.com>
2023-11-15 12:14:17 +0900

    queue

--- python/codeup2.py
+++ python/codeup2.py
@@ -1,7 +1,1 @@
-n = int(input())
-cnt = 0
-for i in [50000, 10000, 5000, 1000, 500, 100, 50, 10]:
-    if n//i != 0:
-        cnt += n//i
-        n = n%i
-print(cnt)
+print(bin(int(input()))[2:])
--- python/queue.py
+++ python/queue.py
@@ -1,11 +1,11 @@
-def createQueue(size):
+def createQueue(size):  
     queue = [None] * size
     front = rear = 0
 
-    def isEmpty():
-        return front == rear
+    def isEmpty(): 
+        return front == rear 
 
-    def isFull():
+    def isFull(): 
         return (front == rear + 1) or (front == 0 and rear == size - 1)
 
     def enqueue(e):
@@ -23,13 +23,18 @@ def createQueue(size):
         else:
             front = (front + 1) % size
             return queue[front]
+        
+    def getQueue():
+        return queue[front +1: rear + 1]
 
-    return enqueue, dequeue
+    return enqueue, dequeue, getQueue
 
-enqueue, dequeue = createQueue(5)
+enqueue, dequeue, getQueue = createQueue(5)
 
 enqueue(1)
 enqueue(2)
 enqueue(3)
+dequeue()
+enqueue(6)
 
-print("Dequeue:", dequeue())
+print("list:", getQueue())
