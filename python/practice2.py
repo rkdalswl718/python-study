e1bf77816fb5913a82bde000679e145ced1d70ed
rkdalswl718 <rkdalswl070718@gmail.com>
2023-12-15 01:36:17 +0900

    stack

---
+++ python/practice2.py
@@ -0,0 +1,46 @@
+class Stack:
+    def __init__(self, stack_size = 5):
+        self.stack = [None] * stack_size
+        self.top = -1
+
+    def isEmpty(self):
+        return self.top == -1
+    
+    def isFull(self):
+        return self.top == len(self.stack) -1
+    
+    def push(self, e):
+        if not self.isFull():
+            self.top += 1
+            self.stack[self.top] = e
+        else :
+            print("꽉찻다임마못너ㅓㅎ는다")
+
+    def pop(self):
+        if not self.isEmpty() :
+            e = self.stack[self.top]
+            self.top -= 1
+            return e
+        else :
+            print('비어잇어서 못 삭제 시킨다ㅋㅋ')
+
+    def peek(self):
+        if not self.isEmpty() :
+            print(self.stack[self.top])
+
+
+stack = Stack()
+
+stack.push(2342)
+stack.push(9)
+stack.push(2)
+stack.push(263)
+
+stack.peek()
+
+stack.push(100)
+stack.push(91)
+stack.push(101)
+stack.pop()
+
+stack.peek()
